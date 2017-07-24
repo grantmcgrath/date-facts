7c4f60ef349d2ca8323303b1876f70c5fc1066f1
Grant McGrath <grantmcgrath@me.com>
2017-07-24 17:13:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 // index.js
 
 const moment = require("moment")
+const chalk = require("chalk")
+var date = moment().format("dddd, MMMM DD0 YYYY, HH:mm:ss a")
+var dayOfYear = moment().format("DDDo")
+var seconds = moment().diff(moment().hour(0).minute(0).second(0), 'seconds')
+
+function dst() {
+  if (moment().isDST() == true) {
+    return chalk.green("is")
+  } return chalk.red("is not")
+}
+
+function lpy() {
+  if (moment().isLeapYear() == true) {
+    return chalk.green("is")
+  } return chalk.red("is not")
+}
+
+console.log(chalk `It is {blue ${date} }.`)
+console.log(chalk `It is the {magenta ${dayOfYear} } day of the year.`)
+console.log(chalk `It is {blue ${seconds} } seconds into the day.`)
+console.log(`It ${dst()} during Dalight Savings Time.`)
+console.log(`It ${lpy()} a leap year.`);
