669fba10302a7b6c53496d7ea2c1d9dadb1c4187
Grant McGrath <grantmcgrath@me.com>
2017-07-25 09:17:36 -0400

    fixed the day of the month to properly display ordinal

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const moment = require("moment")
 const chalk = require("chalk")
-var date = moment().format("dddd, MMMM DD0 YYYY, HH:mm:ss a")
+var date = moment().format("dddd, MMMM Do YYYY, HH:mm:ss a")
 var dayOfYear = moment().format("DDDo")
 var seconds = moment().diff(moment().hour(0).minute(0).second(0), 'seconds')
 
